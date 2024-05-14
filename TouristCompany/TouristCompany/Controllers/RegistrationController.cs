56c66cd096378d09755a673f0ab1727c1cb8d7e9
xarlein <alexvolkovdd@gmail.com>
2024-05-15 01:31:32 +0400

    feat(controllers): регистрация + аутентификация

--- TouristCompany/TouristCompany/Controllers/RegistrationController.cs
+++ TouristCompany/TouristCompany/Controllers/RegistrationController.cs
@@ -6,6 +6,8 @@ using TouristCompany.Models.Entities;
 
 namespace TouristCompany.Controllers
 {
+    // 0fe41223-993b-4df4-b222-e9aa4b5824b4 - Пользователь
+    // a76182e2-7f23-4575-907c-289cbb103ba2 - Администратор
     [ApiController]
     [Route("api/registration")]
     public class RegistrationController(IRepository<User> userRepository) : ControllerBase
@@ -14,6 +16,8 @@ namespace TouristCompany.Controllers
         public IActionResult RegisterUser([FromBody] RegistrationDto registrationDto)
         {
             var newUser = registrationDto.Adapt<User>();
+            newUser.RoleId = Guid.Parse("0fe41223-993b-4df4-b222-e9aa4b5824b4");
+            
             userRepository.Insert(newUser);
 
             return Ok(new
@@ -22,7 +26,8 @@ namespace TouristCompany.Controllers
                 newUser.FirstName,
                 newUser.LastName,
                 newUser.Patronymic,
-                newUser.Email
+                newUser.Email,
+                newUser.RoleId
             });
         }
     }
