ddf4183a648fb4e3aebac934846932537ee17bb8
xarlein <alexvolkovdd@gmail.com>
2024-05-20 18:23:43 +0400

    feat(docker): обновил docker-compose.prod.yml

--- TouristCompany/TouristCompany/Controllers/SearchController.cs
+++ TouristCompany/TouristCompany/Controllers/SearchController.cs
@@ -1,6 +1,18 @@
-﻿namespace TouristCompany.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using TouristCompany.Interfaces;
+using TouristCompany.Models.Entities;
 
-public class SearchController
+namespace TouristCompany.Controllers;
+
+[ApiController]
+[Route("/api/search")]
+public class SearchController(IRepository<Tour> tourRepository) : ControllerBase
 {
-    
+    [HttpGet]
+    public IActionResult Search(string search)
+    {
+        var result = tourRepository.GetAll().Where(i => i.Name.Contains(search)).ToList();
+        
+        return Ok(result);
+    }
 }
