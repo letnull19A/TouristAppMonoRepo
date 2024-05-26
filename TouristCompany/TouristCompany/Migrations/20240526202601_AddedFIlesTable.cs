4c75635ccdd29ee6450baccbe88954396f217ad1
xarlein <alexvolkovdd@gmail.com>
2024-05-27 00:28:35 +0400

    feat(migrations): added migrations

---
+++ TouristCompany/TouristCompany/Migrations/20240526202601_AddedFIlesTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TouristCompany.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFIlesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "files",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    file_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_files", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "files");
+        }
+    }
+}
