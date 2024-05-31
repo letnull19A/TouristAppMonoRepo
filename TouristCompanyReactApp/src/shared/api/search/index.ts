254d6c191a1f921d1399e8849606624cbbcbbae9
xarlein <alexvolkovdd@gmail.com>
2024-05-31 12:08:14 +0400

    fix(search): фикс поиска

--- TouristCompanyReactApp/src/shared/api/search/index.ts
+++ TouristCompanyReactApp/src/shared/api/search/index.ts
@@ -1,3 +1,11 @@
-export const search = async (textSearch: string, airportId: string): Promise<Response> => {
-	return await fetch(`${import.meta.env.VITE_API_URI}/api/search?search=${textSearch}&airportId=${airportId}`)
+export const search = async (
+	textSearch: string,
+	airportId: string
+): Promise<Response> => {
+	return await fetch(
+		`${import.meta.env.VITE_API_URI}/api/search?search=${textSearch}&airportId=${airportId}`,
+		{
+			method: 'POST'
+		}
+	)
 }
