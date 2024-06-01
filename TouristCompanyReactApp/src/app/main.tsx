2bb0e18621fd210be4f590c379be7bd227652260
xarlein <alexvolkovdd@gmail.com>
2024-06-01 22:32:08 +0400

    feat(tickets): обновил базу у авиабилетов

--- TouristCompanyReactApp/src/app/main.tsx
+++ TouristCompanyReactApp/src/app/main.tsx
@@ -4,11 +4,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterApp } from './routes'
 import '/node_modules/primeflex/primeflex.css'
-if (import.meta.env.MODE === 'development') {
-	import('primereact/resources/themes/bootstrap4-dark-blue/theme.css')
-} else {
-	import('primereact/resources/themes/bootstrap4-light-blue/theme.css')
-}
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
