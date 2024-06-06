996921bbac1de376ab0386508f7b73569192e2b7
xarlein <alexvolkovdd@gmail.com>
2024-06-06 13:43:39 +0400

    feat(backend): добавил выдачу рандомных туром с бэка

--- TouristCompany/TouristCompany/Controllers/TourController.cs
+++ TouristCompany/TouristCompany/Controllers/TourController.cs
@@ -56,6 +56,46 @@ namespace TouristCompany.Controllers
             return Ok(result);
         }
 
+        [HttpGet("random/{count:int}")]
+        public IActionResult GetRandom(int count)
+        {
+            var tours = tourRepository.GetAll();
+            var countries = countryRepository.GetAll();
+            var cities = cityRepository.GetAll();
+            var categories = categoryRepository.GetAll();
+
+            var result = tours.Join(countries, u => u.CountryId, v => v.Id, (u, v) => new
+            {
+                Id = u.Id,
+                Name = u.Name,
+                Description = u.Description,
+                CategoryId = u.CategoryId,
+                CityId = u.CityId,
+                ImageUrl = u.ImageUrl,
+                Country = v.Adapt<CountryLiteDto>()
+            }).Join(cities, t => t.CityId, p => p.Id, (t, p) => new
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description,
+                Country = t.Country,
+                ImageUrl = t.ImageUrl,
+                CategoryId = t.CategoryId,
+                City = p.Adapt<CityLiteDto>()
+            }).Join(categories, w => w.CategoryId, q => q.Id, (w, q) => new
+            {
+                w.Id,
+                w.Description,
+                w.Name,
+                w.Country,
+                w.City,
+                w.ImageUrl,
+                Category = categoryRepository.GetById(w.CategoryId).Adapt<CategoryLiteDto>()
+            }).OrderBy(o => Guid.NewGuid()).Take(count);
+            
+            return Ok(result);
+        }
+
         [HttpGet("{id:guid}")]
         public IActionResult GetTourById(Guid id)
         {
