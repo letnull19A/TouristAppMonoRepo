d2bc8f690a3508841474a5e3d561cf06fcbf8c3a
xarlein <alexvolkovdd@gmail.com>
2024-06-09 16:04:36 +0400

    feat(front): добавлено описание при добавлении отеля

--- TouristCompanyReactApp/src/pages/hotel-create/index.tsx
+++ TouristCompanyReactApp/src/pages/hotel-create/index.tsx
@@ -4,6 +4,7 @@ import { CityDropdown, CountryDropdown } from '@ui'
 import { AdminPageTitle } from '@widgets'
 import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext'
+import { InputTextarea } from 'primereact/inputtextarea'
 import { Rating } from 'primereact/rating'
 import { Toast } from 'primereact/toast'
 import { classNames } from 'primereact/utils'
@@ -15,11 +16,11 @@ export const HotelCreate = () => {
 	const [rating, setRating] = useState<number>(0)
 
 	const defaultValues: TAddHotelForm & { countryId: string } = {
-    countryId: '',
+		countryId: '',
 		cityId: '',
 		name: '',
 		description: '',
-		rating: 0,
+		rating: 0
 	}
 
 	const { control, handleSubmit, reset } = useForm({ defaultValues })
@@ -34,7 +35,7 @@ export const HotelCreate = () => {
 		<div className="px-4">
 			<AdminPageTitle title="Добавить новый отель" toMain />
 			<div className="card flex mt-4 col-5">
-				<Toast position='bottom-right' ref={toast} />
+				<Toast position="bottom-right" ref={toast} />
 				<form
 					onSubmit={handleSubmit(onSubmit)}
 					className="flex flex-column gap-40"
@@ -62,7 +63,7 @@ export const HotelCreate = () => {
 					<Controller
 						name="countryId"
 						control={control}
-						render={() => <CountryDropdown className='mt-4' />}
+						render={() => <CountryDropdown className="mt-4" />}
 					/>
 					<Controller
 						name="cityId"
@@ -86,6 +87,24 @@ export const HotelCreate = () => {
 									cancel={false}
 								/>
 							</div>
+						)}
+					/>
+					<Controller
+						name="description"
+						control={control}
+						rules={{ required: 'Описнаие не введено' }}
+						render={({ field, fieldState }) => (
+							<span className="p-float-label my-4">
+								<InputTextarea
+									id={field.name}
+									{...field}
+									rows={4}
+									cols={30}
+									className={classNames({ 'p-invalid': fieldState.error })}
+									style={{ width: '100%' }}
+								/>
+								<label htmlFor={field.name}>Описание</label>
+							</span>
 						)}
 					/>
 					<Button label="Подтвердить" type="submit" icon="pi pi-check" />
